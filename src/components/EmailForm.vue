08c8d4502e3b996436000c8c794a8e718b64fdca
Zac <zacfirst@gmail.com>
2018-04-15 22:15:23 +1000

    feat: add multiple recipient email support

--- src/components/EmailForm.vue
+++ src/components/EmailForm.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="email-form">
+  <form class="email-form" >
+    <div class="badge-container">
+      <div v-for="(email, index) in recipientEmails" :key='index'
+        class="badge badge-info">
+          {{email}}
+      </div>
+    </div>
+
     <div class="input-group mb-3" v-bind:class="{ 'form-group--error': $v.recipient.$error }">
       <div class="input-group-prepend">
         <span class="input-group-text" id="recipient-addon">
@@ -9,12 +16,14 @@
 
       <input type="text" class="form-control"
         v-model.trim="recipient"
+        @keyup="onEmailChange($event, $v)"
+        @change="isRecipientTouched=true"
         @input="$v.recipient.$touch()"
         aria-label="recipient email address"
         aria-describedby="recipient-addon">
     </div>
 
-    <div v-if="$v.recipient.$error && $v.recipient.$dirty"
+    <div v-if="isRecipientTouched && $v.recipient.$error && $v.recipient.$dirty"
       class="alert alert-danger" role="alert">
       <span v-if="!$v.recipient.required">Please enter recipient's email address</span>
       <span v-if="!$v.recipient.email">Please enter a valid email address</span>
@@ -47,22 +56,41 @@
       <textarea class="form-control" rows="8"
         v-model.trim="body"
         @input="$v.body.$touch()"
-        aria-label="email body"
-      ></textarea>
+        aria-label="email body">
+      </textarea>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
 import { email, required } from 'vuelidate/lib/validators'
+
+const EMAIL_RE = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 export default {
   name: 'EmailForm',
   data () {
     return {
       recipient: '',
+      isRecipientTouched: false,
+      recipientEmails: [],
       sender: '',
+      isSenderTouched: false,
+      senderEmails: [],
       body: ''
+    }
+  },
+  methods: {
+    onEmailChange: function (event, $v) {
+      const value = event.target.value
+      const email = value.substring(0, value.length - 1)
+      if (value.includes(',') || value.includes(';')) {
+        if (EMAIL_RE.test(email)) {
+          this.recipientEmails.push(email)
+          this.recipient = ''
+          $v.recipient.$reset()
+        }
+      }
     }
   },
   validations: {
@@ -87,5 +115,12 @@ export default {
 }
 .input-group-text {
   width: 62px;
+}
+.badge-container {
+  display: flex;
+  margin-bottom: 16px;
+}
+.badge {
+  margin-right: 5px;
 }
 </style>
